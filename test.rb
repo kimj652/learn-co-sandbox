78f76730a289cf81c51f1699bfc58c1a5fc87c73
J K <kim.ju652@gmail.com>
2019-08-05 04:46:21 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,16 +1,3 @@
-def total(sum)
-  puts "your total is #{sum}"
-end
-
-def generate
-  sum = 0
-  sum = sum + 5
-  total(sum)
-  return sum
-end
-
-subtotal = generate
-grand = subtotal + 3
-puts "#{grand} is your total."
+def (i, j = nil)
 
 
